0ba2789df78562443e00f15d27d2320e90daef06
A1nu <ainu.develop@gmail.com>
2025-08-25 11:39:00 +0300

    Nvim and terminal tweaks

--- nvim/.config/nvim/lua/a1nu/core/keymaps.lua
+++ nvim/.config/nvim/lua/a1nu/core/keymaps.lua
@@ -30,3 +30,8 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- Scroll down 10 lines and recenter
+keymap.set("n", "<A-j>", "10jzz", { desc = "Scroll down 10 lines and recenter" })
+-- Scroll up 10 lines and recenter
+keymap.set("n", "<A-k>", "10kzz", { desc = "Scroll up 10 lines and recenter" })
