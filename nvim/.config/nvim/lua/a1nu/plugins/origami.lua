388745de560adcfa578d68209eb7a132a3926328
A1nu <ainu.develop@gmail.com>
2025-09-29 11:04:14 +0300

    Swapping to origami

---
+++ nvim/.config/nvim/lua/a1nu/plugins/origami.lua
@@ -0,0 +1,27 @@
+return {
+	"chrisgrieser/nvim-origami",
+	event = "VeryLazy",
+	opts = {
+		useLspFoldsWithTreesitterFallback = true,
+		pauseFoldsOnSearch = true,
+		foldtext = {
+			enabled = true,
+			padding = 3,
+			lineCount = { template = "%d lines", hlgroup = "Comment" },
+			diagnosticsCount = true,
+			gitsignsCount = true,
+		},
+		autoFold = {
+			enabled = true,
+			kinds = { "comment", "imports" },
+		},
+		foldKeymaps = {
+			setup = false,
+			hOnlyOpensOnFirstColumn = false,
+		},
+	},
+	init = function()
+		vim.opt.foldlevel = 99
+		vim.opt.foldlevelstart = 99
+	end,
+}
