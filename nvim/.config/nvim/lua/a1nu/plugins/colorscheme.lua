3bd2de2fca93ba744c55d9779d054a70bbc6c85b
A1nu <ainu.develop@gmail.com>
2025-11-13 11:35:33 +0200

    UI adjustments

--- nvim/.config/nvim/lua/a1nu/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/a1nu/plugins/colorscheme.lua
@@ -6,9 +6,12 @@ return {
 		config = function()
 			-- Optionally configure and load the colorscheme
 			-- directly inside the plugin declaration.
+			vim.g.sonokai_style = "andromeda"
 			vim.g.sonokai_enable_italic = true
 			vim.g.sonokai_transparent_background = 1
 			vim.cmd.colorscheme("sonokai")
+			vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#AF868B", bold = true })
+			vim.api.nvim_set_hl(0, "LineNr", { fg = "#86AFAA" })
 		end,
 	},
 }
