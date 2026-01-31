cbdd351f294e9009be1ead304eab99326da6184e
A1nu <ainu.develop@gmail.com>
2026-01-31 11:08:16 +0200

    Adding ssh cock and work aliases

--- shell/.config/shell/10-env.sh
+++ shell/.config/shell/10-env.sh
@@ -1,1 +1,5 @@
 export EDITOR=nvim
+
+if [[ -z "$SSH_CONNECTION" ]] && [[ -z "$SSH_AUTH_SOCK" ]] && [[ -S "$HOME/.1password/agent.sock" ]]; then
+  export SSH_AUTH_SOCK=~/.1password/agent.sock
+fi
---
+++ shell/.config/shell/21-work-aliases.sh
@@ -0,0 +1,3 @@
+alias nps='npm run grunt start'
+alias npw='npm run grunt watch'
+alias npd='npm run grunt development'
