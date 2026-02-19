fe0ffc5d07b43511d73db135589a94b71048eab3
A1nu <ainu.develop@gmail.com>
2026-02-19 18:24:49 +0200

    1pass env fix

--- shell/.config/shell/10-env.sh
+++ shell/.config/shell/10-env.sh
@@ -1,5 +1,11 @@
 export EDITOR=nvim
 
-if [[ -z "$SSH_CONNECTION" ]] && [[ -z "$SSH_AUTH_SOCK" ]] && [[ -S "$HOME/.1password/agent.sock" ]]; then
-  export SSH_AUTH_SOCK=~/.1password/agent.sock
+if [[ -z "$SSH_CONNECTION" ]]; then
+  if [[ "$SSH_AUTH_SOCK" == %h/* ]]; then
+    export SSH_AUTH_SOCK="$HOME/${SSH_AUTH_SOCK#%h/}"
+  fi
+
+  if [[ -z "$SSH_AUTH_SOCK" ]] && [[ -S "$HOME/.1password/agent.sock" ]]; then
+    export SSH_AUTH_SOCK="$HOME/.1password/agent.sock"
+  fi
 fi
